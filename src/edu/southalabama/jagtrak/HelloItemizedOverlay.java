72481309cb0bf8cf5ed274648d1e4fbffcafd09d
Nasawa <keitonasawa@gmail.com>
2012-02-16 12:40:42 -0600

    First code

---
+++ src/edu/southalabama/jagtrak/HelloItemizedOverlay.java
@@ -0,0 +1,36 @@
+package edu.southalabama.jagtrak;
+
+import java.util.ArrayList;
+
+import android.graphics.drawable.Drawable;
+
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.OverlayItem;
+
+public class HelloItemizedOverlay extends ItemizedOverlay
+{
+
+    private ArrayList<OverlayItem> overlays = new ArrayList<OverlayItem>();
+
+    public HelloItemizedOverlay(Drawable defaultMarker)
+    {
+        super(boundCenterBottom(defaultMarker));
+    }
+
+    protected OverlayItem createItem(int i)
+    {
+        return overlays.get(i);
+    }
+
+    public int size()
+    {
+        return overlays.size();
+    }
+
+    public void addOverlay(OverlayItem overlay)
+    {
+        overlays.add(overlay);
+        populate();
+    }
+
+}
