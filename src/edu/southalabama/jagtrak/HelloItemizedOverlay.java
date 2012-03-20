fb089cfb77c6b3ea775cfdcf6b3a505efa608d34
Hayden Chudy <hjc1710@gmail.com>
2012-03-19 22:01:54 -0500

    Added final prototype, corrected business rules

--- src/edu/southalabama/jagtrak/HelloItemizedOverlay.java
+++ src/edu/southalabama/jagtrak/HelloItemizedOverlay.java
@@ -2,7 +2,10 @@ package edu.southalabama.jagtrak;
 
 import java.util.ArrayList;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.widget.Toast;
 
 import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.OverlayItem;
@@ -10,12 +13,13 @@ import com.google.android.maps.OverlayItem;
 @SuppressWarnings("rawtypes")
 public class HelloItemizedOverlay extends ItemizedOverlay
 {
-
+    Context context;
     private ArrayList<OverlayItem> overlays = new ArrayList<OverlayItem>();
 
-    public HelloItemizedOverlay(Drawable defaultMarker)
+    public HelloItemizedOverlay(Drawable defaultMarker, Context context)
     {
         super(boundCenterBottom(defaultMarker));
+        this.context = context;
     }
 
     protected OverlayItem createItem(int i)
@@ -32,6 +36,17 @@ public class HelloItemizedOverlay extends ItemizedOverlay
     {
         overlays.add(overlay);
         populate();
+    }
+    
+    protected boolean onTap(int x)
+    {
+        OverlayItem item = overlays.get(x);
+        AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+        dialog.setTitle(item.getTitle());
+        dialog.setMessage(item.getSnippet());
+        dialog.show();
+        Toast.makeText(context, "Bob", 1).show();
+        return true;
     }
 
 }
