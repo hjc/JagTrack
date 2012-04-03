f81d82032fe32230a557e67fdd3c2c65eef3d294
Camp Christopher <thectcamp@gmail.com>
2012-04-03 11:05:37 -0500

    fix

---
+++ LCO_Brief_Placeholder/working_code/JAGTrack/src/Coverage.java
@@ -0,0 +1,9 @@
+import java.sql.Time;
+
+//determines when the route is available
+public class Coverage {
+
+	private Time startTime;
+	private Time endTime;
+	
+}
---
+++ LCO_Brief_Placeholder/working_code/JAGTrack/src/Passenger.java
@@ -0,0 +1,9 @@
+
+public class Passenger {
+	private String jagNumber;
+	private String email;
+	private String firstName;
+	private String lastName;
+	private Itinerary itinerary;
+	private Bus currentBus;
+}
