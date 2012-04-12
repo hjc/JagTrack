9699db1a11ef074c1dadc7e88e2ec89c5a96fc99
Hayden Chudy <hjc1710@gmail.com>
2012-04-12 16:35:04 -0500

    Final restructur

---
+++ Artifacts/Management/LCO Brief/working_code/JAGTrack/src/Address.java
@@ -0,0 +1,6 @@
+public class Address{
+	private String streetAddress;
+	private String city;
+	private String state;
+	private String zip;
+}
---
+++ Artifacts/Management/LCO Brief/working_code/JAGTrack/src/Stop.java
@@ -0,0 +1,6 @@
+
+public class Stop {
+	private int stopNumber;
+	private Location location;
+	private Route[] stop_routes;
+}
