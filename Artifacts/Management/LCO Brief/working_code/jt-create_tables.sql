acdb3df410e9fbf80fbc4e396aaa3e4fc6ed8b56
Hayden Chudy <hjc1710@gmail.com>
2012-04-25 17:53:57 -0500

    final table structure with insert data

--- Artifacts/Management/LCO Brief/working_code/jt-create_tables.sql
+++ Artifacts/Management/LCO Brief/working_code/jt-create_tables.sql
@@ -1,3 +1,5 @@
+--could use InnoDB for FK constraints
+
 -- --------------------------------------------------------
 
 --
@@ -22,14 +24,15 @@ CREATE TABLE IF NOT EXISTS `jt_address` (
 CREATE TABLE IF NOT EXISTS `jt_bus` (
   `bus_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'PK',
   `max_seats` int(11) DEFAULT NULL,
-  `arrival_time` date DEFAULT NULL,
-  `departure_time` date DEFAULT NULL,
+  `arrival_time` datetime DEFAULT NULL,
+  `departure_time` datetime DEFAULT NULL,
   `current_route_id` int(11) NOT NULL,
   `current_loc_id` int(11) DEFAULT NULL,
   `next_stop_id` int(11) NOT NULL,
   `passenger_count` int(11) DEFAULT NULL,
   PRIMARY KEY (`bus_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Table for storing bus info. Most NULLs correspond to unknown' AUTO_INCREMENT=1 ;
+
 
 -- --------------------------------------------------------
 
@@ -75,10 +78,11 @@ CREATE TABLE IF NOT EXISTS `jt_passenger` (
 CREATE TABLE IF NOT EXISTS `jt_route` (
   `route_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'PK',
   `color` enum('red','white','blue','yellow','green') DEFAULT NULL COMMENT 'Possible values: red, blue, yellow, green, white',
-  `start_time` date DEFAULT NULL COMMENT 'When route begins running',
-  `end_time` date DEFAULT NULL COMMENT 'When route ends running',
+  `start_time` datetime DEFAULT NULL COMMENT 'When route begins running',
+  `end_time` datetime DEFAULT NULL COMMENT 'When route ends running',
   PRIMARY KEY (`route_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
 
 -- --------------------------------------------------------
 
@@ -104,9 +108,14 @@ CREATE TABLE IF NOT EXISTS `jt_route_stop` (
   `link_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'PK',
   `route_id` int(11) NOT NULL COMMENT 'FK to jt_routes',
   `stop_id` int(11) NOT NULL COMMENT 'FK to jt_stop',
+  `next_stop_id` int(11) NOT NULL COMMENT 'Next stop in this route',
+  `avg_time_to_next_stop` float(11) NOT NULL COMMENT 'avg time from this stop to next one, expressed in minutes; 0.5 = 30s',
+  `distance_to_next_stop` int(11) DEFAULT NULL COMMENT 'distance to next stop, in feet',
   PRIMARY KEY (`link_id`),
   KEY `route_id` (`route_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Table to link routes to their stops.' AUTO_INCREMENT=1 ;
+
+
 
 -- --------------------------------------------------------
 
@@ -117,8 +126,45 @@ CREATE TABLE IF NOT EXISTS `jt_route_stop` (
 CREATE TABLE IF NOT EXISTS `jt_stop` (
   `stop_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'PK',
   `stop_num` int(11) DEFAULT NULL,
-  `loc_id` int(11) NOT NULL COMMENT 'FK to jt_loc, stores stop location',
+  `loc_id` int(11) DEFAULT NULL COMMENT 'FK to jt_loc, stores stop location',   -- should be NOT NULL for release, dont need for proto
   PRIMARY KEY (`stop_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Table for stops' AUTO_INCREMENT=1 ;
+
+
+--------------------------------------------------------------------------------
+----
+  
+-- Insert data
+
+INSERT INTO jt_bus 
+  (max_seats, arrival_time, departure_time, current_route_id, next_stop_id, passenger_count)
+  VALUES (30, NOW(), NOW(), 1, 1, 18),
+  (30, NOW(), NOW(), 1, 2, 18),
+  (30, NOW(), NOW(), 2, 5, 18),
+  (30, NOW(), NOW(), 2, 6, 18),
+  (30, NOW(), NOW(), 2, 7, 18) ;
+  
+  
+INSERT INTO jt_route
+  (color, start_time, end_time)
+  VALUES ('red', '2012-01-04 06:30:00', '2012-01-04 21:30:00'),
+  ('blue', '2012-01-04 06:30:00', '2012-01-04 21:30:00') ;
+  
+INSERT INTO jt_route_stop
+  (route_id, stop_id, next_stop_id, avg_time_to_next_stop)
+  VALUES (1, 1, 2, 6),
+  (1, 2, 3, 8),
+  (1, 3, 4, 12),
+  (1, 4, 1, 4),
+  (2, 5, 6, 8),
+  (2, 6, 7, 12),
+  (2, 7, 8, 4),
+  (2, 8, 1, 9),
+  (2, 1, 5, 4) ;
+  
+INSERT INTO jt_stop
+  (stop_num)
+  VALUES (1),
+  (2), (3), (4), (5), (6), (7), (8) ;
 
 -- --------------------------------------------------------
